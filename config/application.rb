0174addfb1d507bca9664d8bd14ccd394d60e513
Santanu Bhattacharya <santanu.bhattacharya@quantuminventions.com>
2021-04-18 12:36:38 +0530

    trying to fix background images

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,10 @@ module CoTraveller
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-  config.sass.preferred_syntax = :sass
-  config.sass.line_comments = false
-  config.sass.cache = false
+    config.sass.preferred_syntax = :sass
+    config.sass.line_comments = false
+    config.sass.cache = false
+    config.assets.enabled = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
