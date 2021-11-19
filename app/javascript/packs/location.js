cd24435c860ff6566735319e680e9b0ed151e6f9
Santanu Bhattacharya <santanu.bhattacharya@quantuminventions.com>
2021-11-19 19:07:24 +0530

    plotting the locations in the map

--- app/javascript/packs/location.js
+++ app/javascript/packs/location.js
@@ -3,18 +3,72 @@ $(function () {
   const start = $("#start");
   const stop = $("#stop");
 
-  var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
+  const getRandomNumber = function (min, ref) {
+    return Math.random() * ref + min;
+  }
+  var view = new ol.View({
+    center: ol.proj.fromLonLat([88.4306, 22.6138]),
+    zoom: 12
+  });
+
+  // need to remove this from locations in DB
+  var locations = [
+    { lng: 88.4067, lat: 22.6001 },
+    { lng: 88.4306, lat: 22.6138 },
+    { lng: 88.4222, lat: 22.6666 }
+  ];
+
+  var layers = [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ];
+
+  var features = []
+
+  /*$.each(locations, function (index, location) {
+    console.log(location.lat);
+    features.push(new ol.Feature({
+      geometry: new ol.geom.Point(
+        ol.proj.fromLonLat(22.6001, 88.4067)
+      )
+    })
+    )
+  });*/
+
+  for (i = 0; i < locations.length; i++) {
+    features.push(new ol.Feature({
+      geometry: new ol.geom.Point(ol.proj.fromLonLat([
+        locations[i].lng, locations[i].lat
+      ]))
+    }));
+  }
+
+  // create the source and layer for random features
+  const vectorSource = new ol.source.Vector({
+    features
+  });
+
+  const vectorLayer = new ol.layer.Vector({
+    source: vectorSource,
+    style: new ol.style.Style({
+      image: new ol.style.Circle({
+        radius: 3,
+        fill: new ol.style.Fill({ color: 'red' })
       })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([88.4306, 22.6138]),
-      zoom: 10
     })
   });
+
+  // create map and add layers
+  layers.push(vectorLayer);
+
+  var map = new ol.Map({
+    target: 'map',
+    layers: layers,
+    view: view
+  });
+
+
 });
 
 $(document).on("click", "#start", () => {
